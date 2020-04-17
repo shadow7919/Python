1bb197ebc23f5af44811a38254fbeb2ed3d710fe
shadow7919 <malekishayan79@gmail.com>
2020-04-18 02:23:43 +0430

    Update filter and make it to .py

--- filter_func.py
+++ filter_func.py
@@ -1,20 +1,19 @@
-#function that tests if each element of a 
-#sequence true or not.
-#This used in set,lists,touple , ... 
-#and return iterator if its true 
-#It is normally used with func or lambda
+"""
+								filter 
+filter is iterator 
+tests if each element of a 
+sequence true or not.
+This used in set,lists,touple , ... 
+and return if condition is true 
+It is normally used with func or lambda
+"""
 
-
-# 1)
 numbers = [1,2,3,4,5,6,7,8,9,10]
 odd_one_numbers = filter(lambda x : x%2 ==0 ,names)
 print(list(odd_number))
 
-# 2)
 main, strings = input(), []
 n = int(input())
 for i in range(n):
     strings.append(input())
 print(list(filter(lambda word: main in word, strings)))
-
-# 3)
