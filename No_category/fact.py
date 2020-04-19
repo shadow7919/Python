3e87eb93a2fb9a6bfdbc465c1b096beda2626153
shadow7919 <malekishayan79@gmail.com>
2020-04-19 18:23:17 +0430

    some fact help you understand and writ better code

---
+++ No_category/fact.py
@@ -0,0 +1,6 @@
+"""
+																									Facts about python 
+
+In Python, value of an integer is not restricted by the number of bits and can expand to the limit of the available memory
+
+"""
