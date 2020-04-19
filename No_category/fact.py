9bba9dd17b6666e6a0fc84adeb3aea46f34ca21b
shadow7919 <malekishayan79@gmail.com>
2020-04-19 19:10:11 +0430

    some use of _ in python

--- No_category/fact.py
+++ No_category/fact.py
@@ -3,4 +3,10 @@
 
 In Python, value of an integer is not restricted by the number of bits and can expand to the limit of the available memory
 and you have two data type for number integer and long 
+
+Also with big numbers you can sep the with _ like 1_000_000
+It's more readable 
+
+It want to iterat or asign something without use it use _ like variable
+
 """
