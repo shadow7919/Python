9a7aec6b1507a713f4c2ed6b6f340841fd42deac
shadow7919 <malekishayan79@gmail.com>
2020-04-19 18:25:10 +0430

    memory and number data type

--- No_category/fact.py
+++ No_category/fact.py
@@ -2,5 +2,5 @@
 																									Facts about python 
 
 In Python, value of an integer is not restricted by the number of bits and can expand to the limit of the available memory
-
+and you have two data type for number integer and long 
 """
